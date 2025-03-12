bce7350128ccac765b9ccd096f524cc136da50af
PriyankaGoravara <priyagpriya56@gmail.com>
2025-03-12 20:18:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,28 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# IAM Role for EC2 Instances
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
-}
 
 # Security Group for instances
 resource "aws_security_group" "instance_sg" {
