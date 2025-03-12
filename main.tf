5fbaefcead4c0b8b25a1f5579be51717490f48a2
PriyankaGoravara <priyagpriya56@gmail.com>
2025-03-12 20:29:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"] # Only allow traffic from the Load Balancer
+   security_groups = [aws_security_group.lb_sg.id]   # Only allow traffic from the Load Balancer
   }
 
   egress {
